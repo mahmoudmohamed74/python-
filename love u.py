00ab1ff51acd2a41beb02ec3c86e67b6641c9428
mahmoudvolt <101070825+mahmoudvolt@users.noreply.github.com>
2022-03-21 17:52:20 +0200

    Add files via upload

---
+++ love u.py
@@ -0,0 +1,16 @@
+import time
+from turtle import*
+bgcolor("yellow")
+color('red')
+time.sleep(1)
+begin_fill()
+pensize(4)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+time.sleep(1)
+
